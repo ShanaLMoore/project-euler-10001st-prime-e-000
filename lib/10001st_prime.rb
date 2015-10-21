1a7600652ba7021924dfc4c3d123c3e476dfa17f
Shana L Moore <shana.lavina.moore@gmail.com>
2015-10-21 12:47:56 -0700

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,6 @@
 # Implement your procedural solution here!
+require 'Prime'
+
+def prime_number_for(nth_element)
+  Prime.instance.first(nth_element).last
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+
+class Prime
+  require 'Prime'
+
+  def initialize(nth_element)
+    @nth_element = nth_element
+  end
+
+  def number
+    Prime.instance.first(@nth_element).last
+  end
+
+end
